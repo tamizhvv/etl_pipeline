ccceb9262620ad93c81b3b36935e5fedfde02ac3
Tamil <tamizhvv@gmail.com>
2026-03-30 11:17:40 +0530

    added rows count in all functions

--- load.py
+++ load.py
@@ -6,20 +6,26 @@ logger = logging.getLogger('etl_pipeline')
 
 def load_data(data):
     logger.info('Load Started')
-  
-    connection=sqlite3.connect(config.db)
-    cursor=connection.cursor()
+    connection=None
+    try:
+        connection=sqlite3.connect(config.db)
+        cursor=connection.cursor()
 
-    cursor.execute('create table if not exists posts (id integer primary key, userid integer, title text, body text, loaded_at text)')
-    count=0
-    for d in data:
-        try:
-            cursor.execute('''
-                        insert or replace into posts (id,userId,title,body,loaded_at)values (?,?,?,?,?)
-                        ''',(d['id'],d['userId'],d['title'],d['body'],d['loaded_at']))
-            count+=1
-        except Exception as e:
-            logger.error(f'the record failed {d}:{e}')
-    connection.commit()
-    connection.close()
-    logger.info(f'{count} records were loaded and {len(data)-count} were failed ')
+        cursor.execute('create table if not exists posts (id integer primary key, userid integer, title text, body text, loaded_at text)')
+        count=0
+        for d in data:
+            try:
+                cursor.execute('''
+                            insert or replace into posts (id,userId,title,body,loaded_at)values (?,?,?,?,?)
+                            ''',(d['id'],d['userId'],d['title'],d['body'],d['loaded_at']))
+                count+=1
+                
+            except Exception as e:
+                logger.error(f'the record failed {d}:{e}')
+        logger.info(f'{count} records were loaded and {len(data)-count} were failed ')
+        return count
+    finally:
+            if connection:
+                connection.commit()
+                connection.close()
+    
--- main.py
+++ main.py
@@ -1,38 +1,42 @@
+import sqlite3
+import config
+from datetime import datetime
 from extract import extract_data
 from transform import transform_data
 from load import load_data
 from logger import logs
 logger,run_id=logs('etl_pipeline')
-import sqlite3
-import config
-from datetime import datetime
 
 def run_pipeline():
-    connection=sqlite3.connect(config.db)
-    cursor=connection.cursor()
-    cursor.execute('create table if not exists pipeline_runs (run_id text primary key,start_time datetime,end_time datetime,status text,stage text,error_message text)')
-    start_time=datetime.now().isoformat()
-    logger.info('Pipeline Started')
-    raw=extract_data()
-    if raw is None:
-        logger.error('Pipeline aborted at extract')
-        cursor.execute('insert into pipeline_runs (run_id,start_time,end_time,status,stage,error_message)values(?,?,?,?,?,?)',(run_id,start_time,datetime.now().isoformat(),'failed','extract','no values extracted'))
-        connection.commit()
-        connection.close()
-        return
-    cleaned=transform_data(raw)
-    if cleaned is None:
-        logger.error('Pipeline aborted at transform')
-        cursor.execute('insert into pipeline_runs (run_id,start_time,end_time,status,stage,error_message)values(?,?,?,?,?,?)',(run_id,start_time,datetime.now().isoformat(),'failed','transform','no values cleaned'))
-        connection.commit()
-        connection.close()
-        return
-    load_data(cleaned)
-    
-    logger.info('Pipeline Completed')
-    cursor.execute('insert into pipeline_runs (run_id,start_time,end_time,status,stage,error_message)values(?,?,?,?,?,?)',(run_id,start_time,datetime.now().isoformat(),'success',None,None))
-    connection.commit()
-    connection.close()
+    connection=None
+    try:
+        connection=sqlite3.connect(config.db)
+        cursor=connection.cursor()
+        cursor.execute('create table if not exists pipeline_runs (run_id text primary key,start_time datetime,end_time datetime,status text,stage text,error_message text,records_extracted, records_transformed, records_loaded)')
+        start_time=datetime.now().isoformat()
+        logger.info('Pipeline Started')
+        raw=extract_data()
+        
+        if raw is None:
+            logger.error('Pipeline aborted at extract')
+            cursor.execute('insert into pipeline_runs (run_id,start_time,end_time,status,stage,error_message,records_extracted,records_transformed, records_loaded)values(?,?,?,?,?,?,?,?,?)',(run_id,start_time,datetime.now().isoformat(),'failed','extract','no values extracted',0,None,None))
+            return
+        rows_extracted=len(raw)
+        cleaned=transform_data(raw)
+      
+        if cleaned is None:
+            logger.error('Pipeline aborted at transform')
+            cursor.execute('insert into pipeline_runs (run_id,start_time,end_time,status,stage,error_message,records_extracted, records_transformed, records_loaded)values(?,?,?,?,?,?,?,?,?)',(run_id,start_time,datetime.now().isoformat(),'failed','transform','no values cleaned',rows_extracted,0,None))
+            return
+        rows_cleaned=len(cleaned)
+
+        rows_loaded = load_data(cleaned)
+        logger.info('Pipeline Completed')
+        cursor.execute('insert into pipeline_runs (run_id,start_time,end_time,status,stage,error_message,records_extracted, records_transformed, records_loaded)values(?,?,?,?,?,?,?,?,?)',(run_id,start_time,datetime.now().isoformat(),'success',None,None,rows_extracted,rows_cleaned,rows_loaded))
+    finally:
+        if connection:
+            connection.commit()
+            connection.close()
 
 if __name__=='__main__':
     run_pipeline()
